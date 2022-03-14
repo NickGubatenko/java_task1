2be3542b498daebd465501e4717b6a49805374dd
Nick Gubatenko <gubatenkons@gmail.com>
2022-03-14 19:09:02 +0300

    factorial console calc initial commit

--- src/com/lolo/se/hi/MyClass.java
+++ src/com/lolo/se/hi/MyClass.java
@@ -2,10 +2,15 @@ package com.lolo.se.hi;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.Math;
 
 public class MyClass {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter task (1 - factorial, 2 - square root):");
+        int task_selector = Integer.parseInt(br.readLine());
+
         System.out.print("Enter Integer:");
         int number = 0;
         try {
@@ -13,13 +18,22 @@ public class MyClass {
         } catch (NumberFormatException nfe) {
             System.err.println("Invalid Format!");
         }
-        int factorial = 1;
-        if (number > 0) {
-            for (int i = 2; i <= number; i++) {
-                factorial = factorial * i;
-            }
-        }
-        System.out.println(factorial);
-    }
 
+        if (task_selector == 1) { //factorial
+            int factorial = 1;
+            if (number > 0) {
+                for (int i = 2; i <= number; i++) {
+                    factorial = factorial * i;
+                }
+            }
+            System.out.println("factorial = " + factorial);
+
+        } else if (task_selector == 2) { //square root
+            double sq_root = Math.sqrt(number);
+            System.out.println("sqare root of " + number + ": " + sq_root);
+
+        } else { //wrong selector
+            System.err.println("Invalid selector!");
+        }
+    }
 }
