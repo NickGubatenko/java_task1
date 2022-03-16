ba6d1091e51dc67a35a00a4472b1633ef7131aa5
Nick Gubatenko <gubatenkons@gmail.com>
2022-03-16 21:50:28 +0300

    made 2 classes and main

--- src/com/lolo/se/hi/MyClass.java
+++ src/com/lolo/se/hi/MyClass.java
@@ -1,40 +1,14 @@
 package com.lolo.se.hi;
 import java.io.*;
-import java.lang.Math;
 
 public class MyClass {
     public static final UserInterface ui = new UserInterface();
-
-    static void handleTasks() throws IOException {
-        int task = ui.taskSelector();
-        if (task == 1) {
-            calcFactorial(ui.getNumber());
-        } else if (task == 2) {
-            calcSquareRoot(ui.getNumber());
-        } else {
-            ui.showWrongSelectorMessage();
-        }
-    }
-
-    static void calcFactorial(int number) {
-        int factorial = 1;
-        if (number > 0) {
-            for (int i = 2; i <= number; i++) {
-                factorial = factorial * i;
-            }
-        }
-        ui.showResult(factorial);
-    }
-
-    static void calcSquareRoot(int number) {
-        double sq_root = Math.sqrt(number);
-        ui.showResult(sq_root);
-    }
+    public static final TaskExecution te = new TaskExecution(ui);
 
     public static void main(String[] args) throws IOException {
         ui.showLicenseAgreement();
         if (ui.isLicenseAccepted()) {
-            handleTasks();
+            te.handleTasks();
         } else {
             ui.showTerminationMessage();
 
---
+++ src/com/lolo/se/hi/TaskExecution.java
@@ -0,0 +1,39 @@
+package com.lolo.se.hi;
+
+import java.io.IOException;
+import java.lang.Math;
+
+public class TaskExecution {
+    private final UserInterface ui;
+
+    public TaskExecution(UserInterface ui) {
+        this.ui = ui;
+    }
+
+    void handleTasks() throws IOException {
+        int task = ui.taskSelector();
+        if (task == 1) {
+            calcFactorial(ui.getNumber());
+        } else if (task == 2) {
+            calcSquareRoot(ui.getNumber());
+        } else {
+            ui.showWrongSelectorMessage();
+        }
+    }
+
+    void calcFactorial(int number) {
+        int factorial = 1;
+        if (number > 0) {
+            for (int i = 2; i <= number; i++) {
+                factorial = factorial * i;
+            }
+        }
+        ui.showResult(factorial);
+    }
+
+    void calcSquareRoot(int number) {
+        double sq_root = Math.sqrt(number);
+        ui.showResult(sq_root);
+    }
+
+}
